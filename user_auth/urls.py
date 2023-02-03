02cee8fbf622ac4d231a1d3972c14419e5b1abad
DaviesHub <davieschuks.a@gmail.com>
2023-02-03 20:27:56 +0200

    Add urls

---
+++ user_auth/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path, include, re_path
+from . import views
+
+app_name = 'user_auth'
+urlpatterns = [
+    path('', views.user_login, name='user_login'),
+    path('authenticate_user/', views.authenticate_user, name='authenticate_user'),
+    path('home/', views.poll_index, name='home'),
+    path('register/', views.register, name='register'),
+    path('', include('polls.urls')),
+    path('<int:question_id>', views.detail, name='detail'),
+    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'), 
+]
